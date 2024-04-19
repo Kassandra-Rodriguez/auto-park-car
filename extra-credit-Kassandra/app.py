4aad93ab1734f095ba5e9d610904a5e6d786e2f4
Kassandra-Rodriguez <kassandra.rodriguez2014@gmail.com>
2024-04-18 21:19:52 -0600

    changes to app.py

--- extra-credit-Kassandra/app.py
+++ extra-credit-Kassandra/app.py
@@ -8,10 +8,10 @@ def start_drive():
     """Route to start the drive.py script."""
     try:
         # Starting the drive.py script as a subprocess
-        subprocess.Popen(['python3', '/path/to/drive.py'])
+        subprocess.Popen(['python3', '/home/kassandrarodriguez/auto-park-car/drive.py'])
         return "Drive script initiated successfully.", 200
     except Exception as e:
         return f"Failed to start the script: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
